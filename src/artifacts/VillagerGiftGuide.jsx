4e37ef1a01c858b51920e6d05f415f202899563d
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-12 23:44:06 +0530

    message

--- src/artifacts/VillagerGiftGuide.jsx
+++ src/artifacts/VillagerGiftGuide.jsx
@@ -54,7 +54,7 @@ const VillagerGiftGuide = () => {
           setDreamieWishlist(data.dreamies || '');
         }
       } catch (e) {
-        console.log('First time setup');
+        console.log('First time setup', e.message);
       }
     };
     loadData();
